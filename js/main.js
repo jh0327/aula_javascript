fe76dbac9e49efa6b84c7fc799c6d8e04c1665f1
jh0327 <jhony.barros@hotmail.com>
2021-05-22 16:29:44 -0300

    adiciona conteúdo da aula

---
+++ js/main.js
@@ -0,0 +1,112 @@
+//var name = "Jhony Barros";
+//var n1= 30;
+//var n2= 2;
+//alert(n1 + n2);
+//console.log(n1);
+//console.log(n2);
+//console.log(name);
+//var frase= "Japão é o melhor time do mundo";
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//var lista=["maça", "pera", "uva"];
+//lista.push("laranja");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+//var fruta= {nome:"maça", cor:"vermelha"};
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var frutas= [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].cor);
+
+/*
+var idade= prompt("Qual sua idade");
+if (idade >=18){
+	alert("maior de idade");
+}else{
+	alert("menor de idade");
+};
+*/
+/*
+var count=0;
+while (count<=5){
+	console.log(count);
+	count = count +1;
+};
+*/
+/*
+var count;
+for (count=0; count <=5; count++){
+	alert(count);
+};
+*/
+/*
+var d=new Date();
+alert(d.getDay());
+alert(d.getMinutes());
+alert(d.getHours());
+*/
+/*
+function soma(n1, n2){
+	return n1 + n2;
+}
+*/
+/*
+function setReplace(frase, nome, novo_nome){
+	return frase.replace(nome, novo_nome)
+}
+*/
+//alert(soma(5, 10));
+//alert(setReplace("Vai Japao", "Japao", "Brasil"));
+/*
+var validar;
+
+function validaidade(idade){
+if (idade >=18){
+validar=true
+}else{
+validar=false
+}
+return validar;
+}
+
+var idade= prompt("Qual sua idade");
+validaidade(idade);
+console.log(validar);	
+*/
+/*
+function clicou(){
+	document.getElementById("Agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+	//console.log(document.getElementById("Agradecimento"));
+	//alert("Obrigado por clicar");
+}
+
+function Redirecionar(){
+	window.open("https://www.google.com/search?q=tradutor&oq=&aqs=chrome.6.69i59i450l8.1561645882j0j7&sourceid=chrome&ie=UTF-8");
+	//window.location.href="https://www.google.com/search?q=tradutor&oq=&aqs=chrome.6.69i59i450l8.1561645882j0j7&sourceid=chrome&ie=UTF-8";
+}
+
+function trocar(elemento){
+	//document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+	//alert("trocar texto");
+	elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+	//document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+	elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+	alert("Pagina carregada");
+}
+
+function funcaoChenge(elemento){
+	console.log(elemento.value)
+}
+*/
+
